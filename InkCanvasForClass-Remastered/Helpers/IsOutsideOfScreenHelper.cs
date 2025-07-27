e1339fdf15745e08612179720348330399c575cb
Shihao Shen <ssh1579@qq.com>
2025-07-27 08:49:04 +0800

    init
    从icc-re起步

---
+++ InkCanvasForClass-Remastered/Helpers/IsOutsideOfScreenHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Interop;
+
+namespace InkCanvasForClass_Remastered.Helpers
+{
+    internal class IsOutsideOfScreenHelper
+    {
+        public static bool IsOutsideOfScreen(FrameworkElement target)
+        {
+            var hwndSource = (HwndSource)PresentationSource.FromVisual(target);
+            if (hwndSource is null)
+            {
+                return true;
+            }
+
+            var hWnd = hwndSource.Handle;
+            var targetBounds = GetPixelBoundsToScreen(target);
+
+            var screens = System.Windows.Forms.Screen.AllScreens;
+            return !screens.Any(x => x.Bounds.IntersectsWith(targetBounds));
+
+            System.Drawing.Rectangle GetPixelBoundsToScreen(FrameworkElement visual)
+            {
+                var pixelBoundsToScreen = Rect.Empty;
+                pixelBoundsToScreen.Union(visual.PointToScreen(new Point(0, 0)));
+                pixelBoundsToScreen.Union(visual.PointToScreen(new Point(visual.ActualWidth, 0)));
+                pixelBoundsToScreen.Union(visual.PointToScreen(new Point(0, visual.ActualHeight)));
+                pixelBoundsToScreen.Union(visual.PointToScreen(new Point(visual.ActualWidth, visual.ActualHeight)));
+                return new System.Drawing.Rectangle(
+                    (int)pixelBoundsToScreen.X, (int)pixelBoundsToScreen.Y,
+                    (int)pixelBoundsToScreen.Width, (int)pixelBoundsToScreen.Height);
+            }
+        }
+    }
+}
